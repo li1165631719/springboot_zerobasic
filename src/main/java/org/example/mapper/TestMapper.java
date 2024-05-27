a93088f58f96d3db27437ab14d8880cf6821cd48
li1165631719 <li1165631719@users.noreply.github.com>
2024-05-27 20:21:10 +0800

    增加service目录

---
+++ src/main/java/org/example/mapper/TestMapper.java
@@ -0,0 +1,12 @@
+package org.example.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author 李志豪
+ * @create 2024/5/27
+ */
+@Mapper
+public interface TestMapper {
+    
+}
---
+++ src/main/java/org/example/service/TestService.java
@@ -0,0 +1,8 @@
+package org.example.service;
+
+/**
+ * @author 李志豪
+ * @create 2024/5/27
+ */
+public class TestService {
+}
